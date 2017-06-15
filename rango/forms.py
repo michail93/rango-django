0203ab608ca6cdfb8103358c45d4944c61ec91b3
michail93 <shirokov.mixail@gmail.com>
2017-06-15 17:57:40 +0500

    Create forms.py

--- rango/forms.py
+++ rango/forms.py
@@ -11,20 +11,11 @@ class CategoryForm(forms.ModelForm):
         model=Category
         fields=("name",)
 
-# в шаблоне отображаются скрытые("hidden") поля
-# все работет! надо быть внимательнее бля!
+
 class PageForm(forms.ModelForm):
     title=forms.CharField(max_length=128, help_text="Please enter the category name.")
     url=forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views=forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    # не работает, отвергает даже валидные url
-    # def clean(self):
-    #     cleaned_data=self.cleaned_data
-    #     url=cleaned_data.get("url")
-    #     if url and not url.startswith("http://"):
-    #         url="http://"+url
-    #         cleaned_data["url"]=url
-    #         return cleaned_data
     class Meta:
         model=Page
         exclude=('category',)
